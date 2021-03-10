0e72eb0d426b3a048fddb10256221b0fa9b34e18
Marco Antonio <79797841+marcofeliponi@users.noreply.github.com>
2021-03-10 16:30:37 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+var inputNome= document.getElementById("nome");
+localStorage.setItem("nome", inputNome.value);
+
+var inputSobrenome= document.getElementById("sobrenome");
+localStorage.setItem("sobrenome", inputSobrenome.value);
+
+var inputEmail= document.getElementById("email");
+localStorage.setItem("email", inputEmail.value);
+
+var inputSenioridade= document.getElementById("senioridade");
+localStorage.setItem("senioridade", inputSenioridade.value);
+
+var inputExperiencia= document.getElementById("experiencia");
+localStorage.setItem("experiencia", inputExperiencia.value);
+
+var storedValue = localStorage.getItem("frontend");
+
+var storedValue = localStorage.getItem("backend");
+
+var storedValue = localStorage.getItem("fullstack");
+
+var storedValue = localStorage.getItem("HTML");
+
+var storedValue = localStorage.getItem("CSS");
+
+var storedValue = localStorage.getItem("JavaScript");
+
+var storedValue = localStorage.getItem("PHP");
+
+var storedValue = localStorage.getItem("C#");
+
+var storedValue = localStorage.getItem("Python");
+
+var storedValue = localStorage.getItem("Java");
